6c02588cbe534c84fbfc420927fa7c1520c783ab
Gennaro Tortone <gennaro.tortone@na.infn.it>
2022-02-07 09:16:57 +0000

    introduce run control ZMQ socket

--- producer/src/evproducer-mt.cpp
+++ producer/src/evproducer-mt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <chrono>
 #include <thread>
 #include <fmt/core.h>
@@ -17,10 +18,14 @@ unsigned int bufsize = 0;
 // command line arguments
 bool debug = false;
 bool verbose = false;
+bool local = false;
 std::string host;
 int port;
+int mpmtid;
+std::string mode;
 
 zmq::context_t ctx;
+std::string run_state;
 
 void myfree (void *data, void *hint) {
    // we are working with CMA memory, so we don't need
@@ -28,50 +33,83 @@ void myfree (void *data, void *hint) {
    return;
 }
 
+auto control = [](std::string thread_id) {
+
+   zmq::socket_t sock(ctx, zmq::socket_type::sub);
+   const std::string url = fmt::format("tcp://{}:4444", host);
+   sock.connect(url);
+   sock.set(zmq::sockopt::subscribe, "control");
+
+   while(true) {
+
+      zmq::message_t topic;
+      zmq::message_t payload;
+
+      sock.recv(topic);
+      sock.recv(payload);
+
+      if(payload.to_string_view() == "start")
+         run_state = "running";
+      else if(payload.to_string_view() == "stop")
+         run_state = "stopped";
+
+      fmt::print("I: {}\n", payload.to_string_view());
+   }
+};
+
 auto writer = [](std::string thread_id, Udmabuf ub) {
 
    bool sleeping = false;
 
    while(true) {
 
-      // wait if ring buffer is full
-      while(wroff == ((rdoff - rxsize) % bufsize)) {
-         if(debug) {
-            if(!sleeping)
-               fmt::print("W: S\n");
-            sleeping = true;
-            fmt::print(".");
+      if(run_state == "running") {
+
+         // wait if ring buffer is full
+         while(wroff == ((rdoff - rxsize) % bufsize)) {
+            if(debug) {
+               if(!sleeping)
+                  fmt::print("W: S\n");
+               sleeping = true;
+               fmt::print(".");
+            }
+            std::this_thread::sleep_for(100us);
          }
-         std::this_thread::sleep_for(100us);
-      }
 
-      sleeping = false;
+         sleeping = false;
 
-      // S2MM_DA = addr : set destination address for DMA transfer
-      ub.setRegister(S2MM_DESTINATION_ADDRESS, ub.getPhysicalAddress() + wroff);
+         // S2MM_DA = addr : set destination address for DMA transfer
+         ub.setRegister(S2MM_DESTINATION_ADDRESS, ub.getPhysicalAddress() + wroff);
 
-      // S2MM_LENGTH = set buffer size for DMA transfer
-      ub.setRegister(S2MM_LENGTH, rxsize);
+         // S2MM_LENGTH = set buffer size for DMA transfer
+         ub.setRegister(S2MM_LENGTH, rxsize);
 
-      // wait for data ready
-      ub.sync(S2MM_ENDPOINT, 500us);
+         // wait for data ready
+         ub.sync(S2MM_ENDPOINT, 500us);
 
-      wroff = (wroff + rxsize) % bufsize;
+         wroff = (wroff + rxsize) % bufsize;
 
-      if(debug)
-         fmt::print("W: offset={:d}\n", wroff);
+         if(debug)
+            fmt::print("W: offset={:d}\n", wroff);
 
-      if(verbose)
-         ub.getStatus(S2MM_ENDPOINT);
-   }
+         if(verbose)
+            ub.getStatus(S2MM_ENDPOINT);
+
+      } else std::this_thread::sleep_for(1s);
+
+   } 
 };
 
 auto reader = [](std::string thread_id, unsigned char *buffer) {
 
    bool sleeping = false;
-   zmq::socket_t sock(ctx, zmq::socket_type::pair);
-   const std::string url = fmt::format("tcp://{}:{}", host, port);
-   sock.connect(url);
+   zmq::socket_t sock(ctx, zmq::socket_type::dealer);
+
+   if(mode == "remote") {
+      const std::string url = fmt::format("tcp://{}:{}", host, port);
+      sock.set(zmq::sockopt::routing_id, std::to_string(mpmtid));
+      sock.connect(url);
+   }
 
    while(true) {
 
@@ -97,9 +135,11 @@ auto reader = [](std::string thread_id, unsigned char *buffer) {
          fmt::print("\n");
       }
 
-      // send event
-      zmq::message_t z_out(bufusint, rxsize/2, myfree);
-      sock.send(z_out, zmq::send_flags::dontwait);
+      if(mode == "remote") {
+         // send event
+         zmq::message_t z_out(bufusint, rxsize/2, myfree);
+         sock.send(z_out, zmq::send_flags::dontwait);
+      }
 
       rdoff = (rdoff + rxsize) % bufsize;
    
@@ -114,27 +154,34 @@ int main(int argc, const char **argv) {
 
    argparse::ArgumentParser program("evproducer");
 
-   program.add_argument("--debug")
-    .help("enable debug")
+   program.add_argument("--local")
     .default_value(false)
-    .implicit_value(true);
+    .implicit_value(true)
+    .help("local mode");
+
+   program.add_argument("--debug")
+    .default_value(false)
+    .implicit_value(true)
+    .help("enable debug");
 
    program.add_argument("--verbose")
-    .help("print events on stdout")
     .default_value(false)
-    .implicit_value(true);
+    .implicit_value(true)
+    .help("print events on stdout");
 
    program.add_argument("--host")
-    .help("receiver hostname")
-    .default_value(std::string(""))
-    .required();
+    .help("receiver hostname");
    
    program.add_argument("--port")
-    .help("receiver port")
     .default_value(5555)
-    .required();
+    .help("receiver port");
 
-    try {
+   program.add_argument("--id")
+    .help("MPMT id")
+    .default_value(1)
+    .scan<'d', int>();
+
+   try {
       program.parse_args(argc, argv);
    } catch (const std::runtime_error& err) {
       std::cerr << err.what() << std::endl;
@@ -142,10 +189,20 @@ int main(int argc, const char **argv) {
       return EXIT_FAILURE;
    }
 
+   if(program.get<bool>("--local")) {
+      mode = "local";
+   } else if(program.present("--host")) {
+      host = program.get<std::string>("--host");
+      port = program.get<int>("--port");
+      mpmtid = program.get<int>("--id");
+      mode = "remote";
+   } else {
+      std::cerr << "E: specify local or host/id options" << std::endl;
+      return EXIT_FAILURE;
+   }
+      
    debug = program.get<bool>("--debug");
    verbose = program.get<bool>("--verbose");
-   host = program.get<std::string>("--host");
-   port = program.get<int>("--port");
 
    if(verbose)
       fmt::print("I: verbose ON\n");
@@ -192,11 +249,19 @@ int main(int argc, const char **argv) {
    // S2MM_DMACR[15] = 1 : [reserved] - no effect
    ub.setRegister(S2MM_CONTROL_REGISTER, 0xf001);
 
-   std::thread th0 = std::thread(writer, "writer", std::move(ub));
-   std::thread th1 = std::thread(reader, "reader", ub.getBuffer());
+   std::thread th0;
+   if(mode == "remote")
+      th0 = std::thread(control, "control");
+   else run_state = "running";
 
-   th0.join();
+   std::thread th1 = std::thread(writer, "writer", std::move(ub));
+   std::thread th2 = std::thread(reader, "reader", ub.getBuffer());
+
+   if(mode == "remote")
+      th0.join();
+
    th1.join();
+   th2.join();
 
    ub.closeDMABuffer();
 
